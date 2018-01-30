9016dc8035312f500cadc0226c87adebb8581472
Bruno <tanque_250@hotmail.com>
2018-01-30 05:33:27 -0600

    Comenzamos el desarrollo de calificar

---
+++ calificar/calificar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="../images/logo.png">
+	<title>Evaluación</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/estilos.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+
+</body>
+</html>
